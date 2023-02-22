9dc68bb8e495380639addb28dd29ecdb06ec4571
Fırat Altun <frtaltn.psy@gmail.com>
2023-02-22 23:22:38 +0300

    template engine, views-partials

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
+//static files middleware
+app.use(express.static("public"));
+
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
 });
