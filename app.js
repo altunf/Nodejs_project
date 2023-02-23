f0f760b93cde5cf4cfc05d2fdcac4081155ad3eb
Fırat Altun <frtaltn.psy@gmail.com>
2023-02-23 15:00:27 +0300

    worked for .gitignore file

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
