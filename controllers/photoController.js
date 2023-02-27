c5a2c91dc6a7dede594ce8630886ddcc2785add6
Fırat Altun <frtaltn.psy@gmail.com>
2023-02-28 02:34:03 +0300

    homepage editing.

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -48,15 +48,22 @@ const getAllPhotos = async (req, res) => {
 
 const getAPhoto = async (req, res) => {
   try {
-    const photo = await Photo.findById({ _id: req.params.id }).populate("user");
+    const photo = await Photo.findById({ _id: req.params.id }).populate('user');
 
-    res.status(200).render("photo", {
+    let isOwner = false;
+
+    if (res.locals.user) {
+      isOwner = photo.user.equals(res.locals.user._id);
+    }
+
+    res.status(200).render('photo', {
       photo,
-      link: "photos",
+      link: 'photos',
+      isOwner,
     });
   } catch (error) {
     res.status(500).json({
-      succeeded: false,
+      succeded: false,
       error,
     });
   }
